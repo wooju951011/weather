d4df52425082acf111ac8dc5cd1c4970a76b0b29
wooju <dnwnrnt@naver.com>
2022-12-07 23:04:25 +0900

    weather

--- weather/ViewController.swift
+++ weather/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var minTempLabel: UILabel!
+    @IBOutlet weak var maxTempLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var weatherDescriptionLabel: UILabel!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var cityNameTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapFetchWeatherButton(_ sender: UIButton) {
+        if let cityName = self.cityNameTextField.text {
+            self.getCurrentWeather(cityName: cityName)
+            self.view.endEditing(true)
+        }
+    }
+    func getCurrentWeather(cityName: String) {
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=2161918526e2f59a5d559e8d31a44300") else { return }
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: url) {data, response, error in
+            guard let data = data, error == nil else { return }
+            let decoder = JSONDecoder()
+            let weatherInformation = try? decoder.decode(weatherinformation.self, from: data)
+            debugPrint(weatherInformation)
+        }.resume()
+    }
 }
 
---
+++ weather/weatherinformation.swift
@@ -0,0 +1,42 @@
+//
+//  weatherinformation.swift
+//  weather
+//
+//  Created by wooju on 2022/12/07.
+//
+
+import Foundation
+
+struct weatherinformation: Codable {
+    let weather: [Weather]
+    let temp: Temp
+    let name: String
+    
+    
+    enum CodingKeys: String, CodingKey {
+        case weather
+        case temp = "main"
+        case name
+    }
+}
+
+struct Weather: Codable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
+
+struct Temp: Codable {
+    let temp: Double
+    let feelsLike: Double
+    let minTemp: Double
+    let maxTemp: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case minTemp = "temp_min"
+        case maxTemp = "temp_max"
+    }
+}
